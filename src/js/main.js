5ecb020943e3c1baba243a8f3addd1437e41de9b
Studio Starum <dev@starum.studio>
2025-02-02 10:47:42 +0100

    Fix CSS import path and add category filter styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { initNavbar } from './modules/navbar';
 import { initVideoAutoplay } from './modules/videoAutoplay';
 
+import '../styles/category-filters.css';
+
 /**
  * Initialize all modules when DOM is ready
  */
